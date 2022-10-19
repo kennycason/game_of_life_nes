4f347fa96c77135d71f895546c3e0f2d55fa5cab
Kenny Cason <kenneth.cason@gmail.com>
2022-10-19 10:09:59 -0700

    stash game of life work

--- boxxle.c
+++ boxxle.c
@@ -1,7 +1,6 @@
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include <joystick.h>
 #include "lib/neslib/neslib.h"
 
@@ -11,58 +10,82 @@ static uint8_t spr;
 static uint8_t joy0;
 
 // map vars
-#define TILE_SIZE   16
+#define TILE_SIZE   8
 #define WIDTH       256
 #define HEIGHT      240
-#define TILES_X     WIDTH / TILE_SIZE
-#define TILES_Y     HEIGHT / TILE_SIZE
+#define TILES_X     10 // WIDTH / TILE_SIZE
+#define TILES_Y     10 // HEIGHT / TILE_SIZE
 
 static const uint8_t ALIVE = 1;
 static const uint8_t DEAD = 0;
 
 static uint8_t x, y;
 static uint8_t layers[2][TILES_Y][TILES_X] = {
-        { // map[y][x]
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0},
-
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 50, 50, 0, 0,  0, 0, 0, 0, 0, 0},
-
-                {0, 0, 0,  0, 0, 0, 50, 50, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
+        {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         },
-        {// map[y][x]
-                {0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 40},
-                {0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 40},
-                {0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0},
-
-                {0, 0, 6, 0, 0, 0, 1, 0, 0, 0, 0, 0, 40, 0, 0, 0},
-                {0, 0, 7, 0, 0, 0, 1, 0, 0, 0, 0, 40, 0, 0, 0, 0},
-                {0, 0, 8, 0, 0, 0, 1, 0, 0, 0, 40, 0, 0, 0, 0, 0},
-                {0, 0, 9, 0, 0, 0, 0,  0,  0, 40, 0, 0, 0, 0, 0, 0},
-
-                {0, 0, 10, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30}
+        {
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
         }
+//        { // map[y][x]
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0},
+//
+//                {0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0},
+//                {0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0},
+//                {0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0, 0},
+//                {0, 0, 0, 40, 0, 0, 50, 50, 0, 0,  0, 0, 0, 0, 0, 0},
+//
+//                {0, 0, 0,  0, 0, 0, 50, 50, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0,  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+//
+//        },
+//        {// map[y][x]
+//                {0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 40},
+//                {0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 40},
+//                {0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0},
+//
+//                {0, 0, 6, 0, 0, 0, 1, 0, 0, 0, 0, 0, 40, 0, 0, 0},
+//                {0, 0, 7, 0, 0, 0, 1, 0, 0, 0, 0, 40, 0, 0, 0, 0},
+//                {0, 0, 8, 0, 0, 0, 1, 0, 0, 0, 40, 0, 0, 0, 0, 0},
+//                {0, 0, 9, 40, 40, 40, 0,  0,  0, 40, 0, 0, 0, 0, 0, 0},
+//
+//                {0, 0, 10, 0, 0, 0, 0,  0,  0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30}
+//        }
 };
 //static uint8_t (*fg_ptr)[TILES_Y][TILES_X] = &fg;
 
@@ -88,8 +111,8 @@ static uint8_t layers[2][TILES_Y][TILES_X] = {
 //};
 //static uint8_t (*bg_ptr)[TILES_Y][TILES_X] = &bg;
 //static uint8_t (*swap_ptr)[TILES_Y][TILES_X];
-static uint8_t bg_layer = 0;
-static uint8_t fg_layer = 1;
+static uint8_t fg_layer = 0;
+static uint8_t bg_layer = 1;
 static uint8_t neighbors;
 
 uint8_t get(uint8_t x, uint8_t y) {
@@ -110,7 +133,12 @@ void randomize_map() {
             } else {
                 layers[fg_layer][y][x] = DEAD;
             }
-            //   layers[bg_layer][y][x] = DEAD;
+//            if (rand8() > 60) {
+//                layers[bg_layer][y][x] = ALIVE;
+//            } else {
+//                layers[bg_layer][y][x] = DEAD;
+//            }
+            layers[bg_layer][y][x] = DEAD;
         }
     }
 }
@@ -122,25 +150,36 @@ const uint8_t palSprites[16] = {
         0x0f, 0x19, 0x29, 0x39
 };
 
-void swap_layers() {
-    bg_layer = bg_layer == 1 ? 0 : 1;
-    fg_layer = fg_layer == 1 ? 0 : 1;
+
+//put a string into the nametable
+
+void put_str(unsigned int adr,const char *str) {
+    vram_adr(adr);
+    while(1) {
+        if(!*str) break;
+        vram_put((*str++)-0x20);//-0x20 because ASCII code 0x20 is placed in tile 0 of the CHR
+    }
+}
+
+static void swap_layers() {
+    bg_layer = (bg_layer + 1) % 2;
+    fg_layer = (fg_layer + 1) % 2;
 //    swap_ptr = fg_ptr;
 //    fg_ptr = bg_ptr;
 //    bg_ptr = swap_ptr;
 }
+static char str[10];
 
 void main(void) {
-    set_rand(12341);
+    //set_rand(12341);
     pal_spr(palSprites); // set palette for sprites
 
     ppu_on_all(); //enable rendering
 
     randomize_map();
 
-    // music_play(0);
+    music_play(0);
 
-    //swap_layers();
     while (1) {
         ppu_wait_frame();
 
@@ -152,11 +191,16 @@ void main(void) {
 //         //   balls_count++;
 //        }
 
+//        pal_col(1 ,0x30);//set while color
+//        sprintf(str, "FG %d BF %d", fg_layer, bg_layer);
+//        put_str(NTADR_A(2,2), str);
+//        ppu_on_all();//enable rendering
+
         for (y = 0; y < TILES_Y; y++) {
             for (x = 0; x < TILES_X; x++) {
                 if (layers[fg_layer][y][x] > 0) {
-//                    neighbors = count_alive_neighbors(x, y);
-                    spr = oam_spr(x * TILE_SIZE, y * TILE_SIZE, 0x20, 3, spr); // 0x40 is tile number, 1 is palette
+                    neighbors = count_alive_neighbors(x, y);
+                    spr = oam_spr(x * TILE_SIZE, y * TILE_SIZE + 8, layers[fg_layer][y][x], neighbors % 4, spr); // 0x40 is tile number, 1 is palette
                 }
             }
         }
